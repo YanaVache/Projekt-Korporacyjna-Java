104a69c634fdd8d6f46ed4f1c51e1949c3009ba9
YanaVache <yanavache3@gmail.com>
2023-03-29 09:43:29 +0200

    Dodanie obsługi wyjątków dla ujemnych liczb i refaktoryzacja kodu

--- Main.java
+++ Main.java
@@ -3,119 +3,222 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("""
-                --------------------
-                What figure do you want to create?
-                1 - Circle
-                2 - Square
-                --------------------""");
         Scanner scanner = new Scanner(System.in);
-        try {
-            int choice = scanner.nextInt();
-            if (choice == 1) {
-                createCircle();
-            } else if (choice == 2) {
-                createSquare();
-            } else {
-                System.out.println("Wrong number");
+        while (true) {
+            System.out.println("""
+                    --------------------
+                    What figure do you want to create?
+                    1 - Circle
+                    2 - Square
+                    3 - Exit
+                    --------------------""");
+            try {
+                int choice = scanner.nextInt();
+                if (choice == 1) {
+                    createCircle();
+                } else if (choice == 2) {
+                    createSquare();
+                } else if (choice == 3) {
+                    System.out.println("Exiting program...");
+                    break;
+                } else {
+                    System.out.println("Wrong number");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input");
+                scanner.next();
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Wrong input");
         }
     }
 
     private static void createCircle() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("""
-                --------------------
-                How do you want to create a circle?
-                1 - From Radius
-                2 - From Diameter
-                3 - From Circumference
-                4 - From Area
-                --------------------""");
-        try {
-            int option = scanner.nextInt();
-            switch (option) {
-                case 1 -> {
-                    System.out.println("""
-                            --------------------
-                            Enter Radius
-                            --------------------""");
-                    Circle circle = new Circle(scanner.nextDouble(), option); // 40. i 41. linijka się powtarzają
-                    System.out.println(circle);                               // Potrzebna drobna refaktoryzacja
-                }                                                             // Wywalić do osobnej metody?
-                case 2 -> {                                                   // Albo stworzyć nową w klasie Circle
-                    System.out.println("""
-                            --------------------
-                            Enter Diameter
-                            --------------------""");
-                    Circle circle = new Circle(scanner.nextDouble(), option);
-                    System.out.println(circle);
+        while (true) {
+            System.out.println("""
+                    --------------------
+                    How do you want to create a circle?
+                    1 - From Radius
+                    2 - From Diameter
+                    3 - From Circumference
+                    4 - From Area
+                    --------------------""");
+            try {
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1 -> {
+                        do {
+                            System.out.println("""
+                                    --------------------
+                                    Enter Radius
+                                    --------------------""");
+                            while (!scanner.hasNextDouble()) {
+                                System.out.println("Wrong input. Enter a number:");
+                                scanner.next();
+                            }
+                            double radius = scanner.nextDouble();
+                            if (radius < 0) {
+                                System.out.println("Radius cannot be negative");
+                                break;
+                            } else {
+                                Circle circle = new Circle(radius, option);
+                                System.out.println(circle);
+                                return;
+                            }
+                        } while (true);
+                    }
+                    case 2 -> {
+                        do {
+                            System.out.println("""
+                                    --------------------
+                                    Enter Diameter
+                                    --------------------""");
+                            while (!scanner.hasNextDouble()) {
+                                System.out.println("Wrong input. Enter a number:");
+                                scanner.next();
+                            }
+                            double diameter = scanner.nextDouble();
+                            if (diameter < 0) {
+                                System.out.println("Diameter cannot be negative");
+                                break;
+                            } else {
+                                Circle circle = new Circle(diameter, option);
+                                System.out.println(circle);
+                                return;
+                            }
+                        } while (true);
+                    }
+                    case 3 -> {
+                        do {
+                            System.out.println("""
+                                    --------------------
+                                    Enter Circumference
+                                    --------------------""");
+                            while (!scanner.hasNextDouble()) {
+                                System.out.println("Wrong input. Enter a number:");
+                                scanner.next();
+                            }
+                            double circumference = scanner.nextDouble();
+                            if (circumference < 0) {
+                                System.out.println("Circumference cannot be negative");
+                                break;
+                            } else {
+                                Circle circle = new Circle(circumference, option);
+                                System.out.println(circle);
+                                return;
+                            }
+                        } while (true);
+                    }
+                    case 4 -> {
+                        do {
+                            System.out.println("""
+                                    --------------------
+                                    Enter Area
+                                    --------------------""");
+                            while (!scanner.hasNextDouble()) {
+                                System.out.println("Wrong input. Enter a number:");
+                                scanner.next();
+                            }
+                            double area = scanner.nextDouble();
+                            if (area < 0) {
+                                System.out.println("Area cannot be negative");
+                                break;
+                            } else {
+                                Circle circle = new Circle(area, option);
+                                System.out.println(circle);
+                                return;
+                            }
+                        } while (true);
+                    }
+                    default -> System.out.println("Wrong number");
                 }
-                case 3 -> {
-                    System.out.println("""
-                            --------------------
-                            Enter Circumference
-                            --------------------""");
-                    Circle circle = new Circle(scanner.nextDouble(), option);
-                    System.out.println(circle);
-                }
-                case 4 -> {
-                    System.out.println("""
-                            --------------------
-                            Enter Area
-                            --------------------""");
-                    Circle circle = new Circle(scanner.nextDouble(), option);
-                    System.out.println(circle);
-                }
-                default -> System.out.println("Wrong number");
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input");
+                scanner.nextLine();
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Wrong input");
         }
     }
 
     private static void createSquare() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("""
-                --------------------
-                How do you want to create a square?
-                1 - From Side Length
-                2 - From Diagonal
-                3 - From Area
-                --------------------""");
-        try {
-            int option = scanner.nextInt();
-            switch (option) {
-                case 1 -> {
-                    System.out.println("""
-                            --------------------
-                            Enter Side Length
-                            --------------------""");
-                    Square square = new Square(scanner.nextDouble(), option);
-                    System.out.println(square);
+        while (true) {
+            System.out.println("""
+                    --------------------
+                    How do you want to create a square?
+                    1 - From Side Length
+                    2 - From Diagonal
+                    3 - From Area
+                    --------------------""");
+            try {
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1 -> {
+                        do {
+                            System.out.println("""
+                                    --------------------
+                                    Enter Side Length
+                                    --------------------""");
+                            while (!scanner.hasNextDouble()) {
+                                System.out.println("Wrong input. Enter a number:");
+                                scanner.next();
+                            }
+                            double sideLength = scanner.nextDouble();
+                            if (sideLength < 0) {
+                                System.out.println("Side length cannot be negative");
+                            } else {
+                                Square square = new Square(sideLength, option);
+                                System.out.println(square);
+                                return;
+                            }
+                        } while (true);
+
+                    }
+                    case 2 -> {
+                        do {
+                            System.out.println("""
+                                    --------------------
+                                    Enter Diagonal
+                                    --------------------""");
+                            while (!scanner.hasNextDouble()) {
+                                System.out.println("Wrong input. Enter a number:");
+                                scanner.next();
+                            }
+                            double diagonal = scanner.nextDouble();
+                            if (diagonal < 0) {
+                                System.out.println("Diagonal cannot be negative");
+                                break;
+                            } else {
+                                Square square = new Square(diagonal, option);
+                                System.out.println(square);
+                                return;
+                            }
+                        } while (true);
+                    }
+                    case 3 -> {
+                        do {
+                            System.out.println("""
+                                    --------------------
+                                    Enter Area
+                                    --------------------""");
+                            while (!scanner.hasNextDouble()) {
+                                System.out.println("Wrong input. Enter a number:");
+                                scanner.next();
+                            }
+                            double area = scanner.nextDouble();
+                            if (area < 0) {
+                                System.out.println("Area cannot be negative");
+                            } else {
+                                Square square = new Square(area, option);
+                                System.out.println(square);
+                                return;
+                            }
+                        } while (true);
+                    }
+                    default -> System.out.println("Wrong number");
                 }
-                case 2 -> {
-                    System.out.println("""
-                            --------------------
-                            Enter Diagonal
-                            --------------------""");
-                    Square square = new Square(scanner.nextDouble(), option);
-                    System.out.println(square);
-                }
-                case 3 -> {
-                    System.out.println("""
-                            --------------------
-                            Enter Area
-                            --------------------""");
-                    Square square = new Square(scanner.nextDouble(), option);
-                    System.out.println(square);
-                }
-                default -> System.out.println("Wrong number");
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input");
+                scanner.nextLine();
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Wrong input");
         }
     }
 }
