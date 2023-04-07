9cd477259717818483d7c525dd6ca81329666d2b
Patryk Mazur <64105446+Papunio@users.noreply.github.com>
2023-04-07 10:53:54 +0200

    Zmiana organizacji projektu i takie tam (Part2..)

---
+++ src/Program.java
@@ -0,0 +1,195 @@
+import figures.Circle;
+import figures.Rectangle;
+import figures.Rhombus;
+import figures.Square;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public void runProgram() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("""
+                    --------------------
+                    What figure do you want to create?
+                    1 - Circle
+                    2 - Square
+                    3 - Rectangle
+                    4 - Rhombus
+                    5 - Exit
+                    --------------------""");
+            try {
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1 -> createCircle(scanner);
+                    case 2 -> createSquare(scanner);
+                    case 3 -> createRectangle(scanner);
+                    case 4 -> createRhombus(scanner);
+                    case 5 -> {
+                        System.out.println("Exiting program...");
+                        return;
+                    }
+                    default -> System.out.println("Wrong number");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input");
+                scanner.next();
+            }
+        }
+    }
+
+    private void createFigure(Scanner scanner, int option, int args, String[] s, String figureToCreate) {
+        double[] values = new double[args];
+        for (int i = 0; i < args; i++) {
+            System.out.printf("""
+                    --------------------
+                    Enter %s
+                    --------------------%n""", s[i]);
+            double value = scanner.nextDouble();
+            if (value <= 0) {
+                System.out.printf("%s has to be greater than zero%n", s[i]);
+                return;
+            }
+            values[i] = value;
+        }
+    
+        switch (figureToCreate) {
+            case "Circle" -> {
+                Circle circle = new Circle(values[0], option);
+                System.out.println(circle);
+            }
+            case "Square" -> {
+                Square square = new Square(values[0], option);
+                System.out.println(square);
+            }
+            case "Rectangle" -> {
+                Rectangle Rectangle = new Rectangle(values[0], values[1], option);
+                System.out.println(Rectangle);
+            }
+            case "Rhombus" -> {
+                Rhombus rhombus = new Rhombus(values[0], values[1], option);
+                System.out.println(rhombus);
+            }
+            default -> throw new IllegalArgumentException("");
+        }
+    }
+
+    private void createCircle(Scanner scanner) {
+        while (true) {
+            System.out.println("""
+                    --------------------
+                    How do you want to create a circle?
+                    1 - From Radius
+                    2 - From Diameter
+                    3 - From Circumference
+                    4 - From Area
+                    5 - Go back
+                    --------------------""");
+            try {
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1 -> createFigure(scanner, option, 1, new String[]{"Radius"}, "Circle");
+                    case 2 -> createFigure(scanner, option, 1, new String[]{"Diameter"}, "Circle");
+                    case 3 -> createFigure(scanner, option, 1, new String[]{"Circumference"}, "Circle");
+                    case 4 -> createFigure(scanner, option, 1, new String[]{"Area"}, "Circle");
+                    case 5 -> {
+                        return;
+                    }
+                    default -> System.out.println("Wrong number");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input, enter a positive number");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private void createSquare(Scanner scanner) {
+        while (true) {
+            System.out.println("""
+                    --------------------
+                    How do you want to create a square?
+                    1 - From Side Length
+                    2 - From Diagonal
+                    3 - From Area
+                    4 - Go back
+                    --------------------""");
+            try {
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1 -> createFigure(scanner, option, 1, new String[]{"Side length"}, "Square");
+                    case 2 -> createFigure(scanner, option, 1, new String[]{"Diagonal"}, "Square");
+                    case 3 -> createFigure(scanner, option, 1, new String[]{"Area"}, "Square");
+                    case 4 -> {
+                        return;
+                    }
+                    default -> System.out.println("Wrong number");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private void createRhombus(Scanner scanner){
+        while (true) {
+            System.out.println("""
+                    --------------------
+                    How do you want to create a rhombus?
+                    1 - From Diagonals
+                    2 - From Diagonal and Area
+                    3 - From Diagonal and Side Length
+                    4 - From Area and Side Length
+                    5 - Go back
+                    --------------------""");
+            try {
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1 -> createFigure(scanner, option, 2, new String[]{"First Diagonal", "Second Diagonal"}, "Rhombus");
+                    case 2 -> createFigure(scanner, option, 2, new String[]{"Diagonal", "Area"}, "Rhombus");
+                    case 3 -> createFigure(scanner, option, 2, new String[]{"Diagonal", "Side Length"}, "Rhombus");
+                    case 4 -> createFigure(scanner, option, 2, new String[] { "Area", "Side Length" }, "Rhombus");
+                    case 5 -> {
+                        return;
+                    }
+                    default -> System.out.println("Wrong number");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private void createRectangle(Scanner scanner) {
+        while (true) {
+            System.out.println("""
+                    --------------------
+                    How do you want to create a figures.Rectangle?
+                    1 - From Side Lengths
+                    2 - From Diagonal and Side Length
+                    3 - From Area and Side Length
+                    4 - From Area and Diagonal
+                    5 - Go back
+                    --------------------""");
+            try {
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1 -> createFigure(scanner, option, 2, new String[]{"Side length 1", "Side length 2"}, "Rectangle");
+                    case 2 -> createFigure(scanner, option, 2, new String[]{"Diagonal", "Side length"}, "Rectangle");
+                    case 3 -> createFigure(scanner, option, 2, new String[]{"Area", "Side length"}, "Rectangle");
+                    case 4 -> createFigure(scanner, option, 2, new String[]{"Area", "Diagonal"}, "Rectangle");
+                    case 5 -> {
+                        return;
+                    }
+                    default -> System.out.println("Wrong number");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Wrong input");
+                scanner.nextLine();
+            }
+        }         
+    }
+}
---
+++ src/figures/Rhombus.java
@@ -0,0 +1,93 @@
+package figures;
+
+public class Rhombus extends Figure {
+    private double length;
+    private double area;
+    private double diagonalFst;
+    private double diagonalSnd;
+
+    public Rhombus(double a, double b, int option) {
+        switch (option) {
+            case 1 -> calculateFromDiagonals(a, b);
+            case 2 -> calculateFromDiagonalAndArea(a, b);
+            case 3 -> calculateFromDiagonalAndSideLength(a, b);
+            case 4 -> calculateFromAreaAndSideLength(a, b);
+            default -> System.out.println("figures.Rhombus: Option Error");
+        }
+    }
+
+    private void calculateFromDiagonals(double rDiagonalFst, double rDiagonalSnd) {
+        this.diagonalFst = rDiagonalFst;
+        this.diagonalSnd = rDiagonalSnd;
+
+        this.length = Math.sqrt(Math.pow(rDiagonalSnd / 2, 2) + Math.pow(rDiagonalFst / 2, 2));
+        this.area = (rDiagonalFst * rDiagonalSnd) / 2;
+    }
+
+    private void calculateFromDiagonalAndArea(double rDiagonal, double rArea) {
+        this.area = rArea;
+        this.diagonalFst = rDiagonal;
+        this.diagonalSnd = 2 * rArea / rDiagonal;
+
+        this.length = Math.sqrt(Math.pow(diagonalFst / 2, 2) + Math.pow(diagonalSnd / 2, 2));
+    }
+
+    private void calculateFromDiagonalAndSideLength(double rDiagonal, double rLength) {
+        this.diagonalFst = rDiagonal;
+        this.length = rLength;
+
+        this.diagonalSnd = Math.sqrt(Math.pow(rLength, 2) - Math.pow(rDiagonal / 2, 2)) * 2;
+        this.area = (this.diagonalFst * this.diagonalSnd) / 2;
+    }
+
+    private void calculateFromAreaAndSideLength(double rArea, double rLength){
+        this.area = rArea;
+        this.length = rLength;
+
+        double a = 1;
+        double b = -4 * Math.pow(this.length, 2);
+        double c = 4 * Math.pow(this.area, 2);
+
+        double delta = Math.pow(b, 2) - (4 * a * c);
+
+        if (delta >= 0) {
+            double d2Squared1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double d2Squared2 = (-b - Math.sqrt(delta)) / (2 * a);
+
+            double d2Squared = Math.max(d2Squared1, d2Squared2);
+            double d2 = Math.sqrt(d2Squared);
+            double d1 = (2 * this.area) / d2;
+
+            this.diagonalFst = d1;
+            this.diagonalSnd = d2;
+        } else {
+            System.out.println("Invalid input, the area and side length combination is not possible for a rhombus.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "--------------------"
+                + "\nfigures.Rhombus"
+                + "\nSide Length: " + String.format("%.2f", this.length)
+                + "\nFirst Diagonal: " + String.format("%.2f", this.diagonalFst)
+                + "\nSecond Diagonal: " + String.format("%.2f", this.diagonalSnd)
+                + "\nArea: " + String.format("%.2f", this.area);
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getDiagonalFst() {
+        return this.diagonalFst;
+    }
+
+    public double getDiagonal() {
+        return this.diagonalSnd;
+    }
+
+    public double getArea() {
+        return this.area;
+    }
+}
